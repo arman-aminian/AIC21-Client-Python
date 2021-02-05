d4d676eebc50cf2eb58e4e610c560795be86df77
m.dehghankar <mohsendehghankar@gmail.com>
2021-02-05 20:17:53 +0330

    model changes

--- Model.py
+++ Model.py
@@ -1,49 +1,74 @@
+ANT_TYPES = ["SARBAAZ", "KARGAR"]
+CELL_TYPES = ["EMPTY", "WALL", "BASE"]
+RESOURCE_TYPES = ["BREAD", "GRASS"]
+DIRECTIORN = ["RIGHT", "LEFT", "UP", "DOWN", "CENTER"]
+
+
 class Ant:
-	def __init__(self, type, health, locationCell)
-		self.type = type
-		self.locationCell = locationCell
-		self.health = health
+    def __init__(self, type, health, locationCell):
+        # ANT_TYPES
+        self.type = type
+        # Current Cell
+        self.locationCell = locationCell
+        self.health = health
 
-	def move(self):
-		pass
+    # returns a DIRECTION
+    def move(self):
+        pass
 
-	def send_message(self, text):
-		pass
+    def send_message(self, text):
+        pass
 
-	def get_location(self):
-		return self.locationCell
+    # get current location
+    def get_location(self):
+        return self.locationCell
 
-	def get_neighbours(self):
-		pass
+    # get cells in this ants view
+    def get_neighbours_cell(self):
+        pass
 
-	def get_health(self):
-		return self.health
+    def get_health(self):
+        return self.health
+
 
 class Map:
-	def __init__(self, length, width):
-		self.length = length
-		self.width = width
-		self.cells = None
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+        self.cells = None
+
 
 class Cell:
-	def __init__(self, x, y, type):
-		self.x = x
-		self.y = y
-		self.type = type
+    def __init__(self, x, y, type):
+        self.x = x
+        self.y = y
+        # CELL_TYPES
+        self.type = type
+		# current ants in this cell
+        self.ants = []
 
 
 class Resource:
-	def __init__(self, type, amount)
-		self.type = type
-		self.amount = amount
+    def __init__(self, type, amount):
+        # RESOURCE_TYPES
+        self.type = type
+        self.amount = amount
 
-	def get_type(self)
-		return self.type
+    def get_type(self):
+        return self.type
 
-	def get_amount(self)
-		return self.amount
+    def get_amount(self):
+        return self.amount
 
+# CurrentState: each turn before AI.py is called
+# GameConfig: getting from server at startup
 
-class Player:
-	def __init__(self):
-		pass
+# def AI(self, currentState):
+# 	#
+# 	#
+# 	#
+
+# 	return {
+# 		Message,
+# 		Direction,
+# 	}
