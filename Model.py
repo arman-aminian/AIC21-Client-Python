6e4f2386c91c0b98cbf6e250df1b20d3b9209d86
Sahar Shayegan <saharomidishayegan@gmail.com>
2021-02-05 19:18:34 +0330

    Add methods

--- Model.py
+++ Model.py
@@ -1,15 +1,49 @@
 class Ant:
-	def __init__:
+	def __init__(self, type, health, locationCell)
+		self.type = type
+		self.locationCell = locationCell
+		self.health = health
+
+	def move(self):
 		pass
+
+	def send_message(self, text):
+		pass
+
+	def get_location(self):
+		return self.locationCell
+
+	def get_neighbours(self):
+		pass
+
+	def get_health(self):
+		return self.health
 
 class Map:
-	def __init__:
-		pass
+	def __init__(self, length, width):
+		self.length = length
+		self.width = width
+		self.cells = None
 
 class Cell:
-	def __init__:
-		pass
+	def __init__(self, x, y, type):
+		self.x = x
+		self.y = y
+		self.type = type
+
 
 class Resource:
-	def __init__:
+	def __init__(self, type, amount)
+		self.type = type
+		self.amount = amount
+
+	def get_type(self)
+		return self.type
+
+	def get_amount(self)
+		return self.amount
+
+
+class Player:
+	def __init__(self):
 		pass
