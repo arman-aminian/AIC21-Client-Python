6e90a51edda06ba5a970a2ab6b9e98954ef6fa3f
Sahar Shayegan <saharOmidiShayegan@gmail.com>
2021-02-08 18:39:24 +0330

    Add init and turn method

--- AI.py
+++ AI.py
@@ -1,1 +1,16 @@
 
+from model import *
+
+class AI:
+    def __init__(self, currentState):
+        message = None # type: str
+        direction = None # type: DIRECTION
+        self.currentState = currentState
+
+# this will be called each turn and it should return a message and a direction
+    def turn(self):
+
+    	return {
+    		message,
+    		direction,
+    	}
