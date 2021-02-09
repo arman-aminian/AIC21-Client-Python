0777dbfec8139927370287178ebaf71fcb6e28fc
Sahar Shayegan <saharOmidiShayegan@gmail.com>
2021-02-09 19:04:01 +0330

    Improve AI

--- AI.py
+++ AI.py
@@ -1,16 +1,28 @@
-
-from model import *
+from Model import *
+from Controller import *
+import random
 
 class AI:
-    def __init__(self, currentState):
-        message = None # type: str
-        direction = None # type: DIRECTION
+    def __init__(self):
+        self.message = None # type: str
+        self.direction = None # type: DIRECTION
+        self.currentState = None # type: CurrentState
+        self.gameConfig = None #type: GameConfig
+
+    # this will be called in controller
+    def set_current_state(self, currentState):
         self.currentState = currentState
 
-# this will be called each turn and it should return a message and a direction
+    # this will be called in controller
+    def set_game_config(self, gameConfig):
+        self.gameConfig = gameConfig
+
+    # this will be called each turn and it should return message and direction
     def turn(self):
 
+        self.direction = random.choice(Direction)
+        self.message = ""
     	return {
-    		message,
-    		direction,
+    		self.message,
+    		self.direction,
     	}
