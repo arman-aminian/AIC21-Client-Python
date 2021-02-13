7b55291c58fabe61cfbb34c2025301b6389f71ea
mohadeseghafari <62194136+mohadeseghafari@users.noreply.github.com>
2021-02-14 00:20:17 +0330

    handle new api

--- Controller.py
+++ Controller.py
@@ -4,87 +4,125 @@ import threading
 import traceback
 from queue import Queue
 from threading import Thread
-from models import Map
 from AI import AI
 from network import Network
-from world import World
+from model import
+
+
+class Message:
+    def __init__(self, turn, type, info):
+        self.type = type
+        self.info = Info(info)
+        self.turn = turn
+       
+
+    
+class Info:
+    def __info__(self, inf):
+        self.
+        
+class ServerConstants:
+    KEY_INFO = "info"
+    KEY_TURN = "turn"
+    KEY_TYPE = "type"
+
+    CONFIG_KEY_IP = "ip"
+    CONFIG_KEY_PORT = "port"
+    CONFIG_KEY_TOKEN = "token"
+
+
+#     MESSAGE_TYPE_DIRECTION = 1
+#     MESSAGE_TYPE_CHAT = 2
+    MESSAGE_TYPE_INIT = 3
+    MESSAGE_TYPE_TURN = 4
+    
+
+
+
 
 class Controller:
 
     def __init__(self):
         self.sending_flag = True
-        self.conf = GameConfig()
+        self.gameConfig = None
+        self.conf = []
         self.network = None
         self.queue = Queue()
-        self.map = Map()
         self.client = AI()
         self.argNames = ["AICHostIP", "AICHostPort", "AICToken", "AICRetryDelay"]
         self.argDefaults = ["127.0.0.1", 7099, "00000000000000000000000000000000", "1000"]
         self.turn_num = 0
 
-    def start():
+    
+    # fill args in threads
+    
+    def handle_message(self, message):
+        if message[ServerConstants.KEY_TYPE] == ServerConstants.MESSAGE_TYPE_INIT:
+            self.handle_init_message(message[ServerConstants.KEY_INFO])
+            threading.Thread().start()
+
+        elif message[ServerConstants.KEY_TYPE] == ServerConstants.MESSAGE_TYPE_TURN:
+            self.handle_turn_message(message[ServerConstants.KEY_INFO])
+            threading.Thread().start()
+            
+
+
+        
+
+            
+    def send_direction_message(self, direction):
+        self.network.send({
+                           "type": 1,
+                           "info": {"direction": direction}
+                            })
+    
+    def send_chat_message(self, chat,value):
+        self.network.send({
+                           "type": 2,
+                           "info": {"message": chat,
+                                   "value" : value
+                                   }
+                            })
+    
+    def send_end_message():
+        self.network.send({
+                           "type": 6,
+                           "info": {}
+                            })
+    
+    def handle_turn_message(self, message):
+        
+        pass
+    
+    def handle_init_message(self, massage):
+        
+        self.gameConfig = GameConfig(list(message.values()))
+            
+        
+
+    def start(self):
+        self.read_settings()
         self.network = Network(ip=self.conf[self.argNames[0]],
-                               port=self.conf[self.argNames[1]],
+                               port=int(self.conf[self.argNames[1]]),
                                token=self.conf[self.argNames[2]],
                                message_handler=self.handle_message)
         self.network.connect()
+        Thread().start()
+        
+
+
+    def read_settings(self):
+        if os.environ.get(self.argNames[0]) is None:
+            for i in range(len(self.argNames)):
+                self.conf[self.argNames[i]] = self.argDefaults[i]
+        else:
+            for i in range(len(self.argNames)):
+                self.conf[self.argNames[i]] = os.environ.get(self.argNames[i])
 
     def terminate(self):
         print("finished!")
         self.network.close()
         self.sending_flag = False
 
-    def run():
-            while self.sending_flag:
-                event = self.queue.get()
-                self.queue.task_done()
-                message = handle_map_massage()
-                self.network.send(message)
-
-    def turn(gameConfig, massage):
-        self.client.set_currrent_state()
-        
-        
-    def handle_chat(massage):
-        pass
-
-    def handle_direction(massage):
-        self.client.set_direction(massage.direction)
-
-    def handle_init_massage(massage):
-      self.client.set_current_state(massage)
-        
-
-    def handle_map_massage():
-     return {
-                    "map_width": self.conf.map_width,
-                    "map_heigth": self.conf.map_height,
-                    "ant_type": self.conf.ant_type, 
-                    "base_x": self.conf.base_x,  
-                    "base_y": self.conf.base_y,
-                    "health_kargar": self.conf.health_kargar,
-                    "health_sarbaaz": self.conf.health_sarbaz,
-                    "attack_distance":self.conf.attack_distance,
-                    "generate_kargar": self.conf.generate_kargar,
-                    "generate_sarbaaz": self.conf.generate_sabaz,
-                    "rate_death_resource": self.conf.rate_death_resource
-                }
-      
-
-    def handle_message(self, massage):
-      # this need changes
-      if (massage.type == 0) :
-         self.handle_init_massage()
-      elif (massage.type == 1) :
-          self.handle_direction(massage)
-      elif (massage.type == 2) :
-          self.handle_chat(massage)
-      pass
 
 
-
-
-
-
-c = Controller()
-c.start()
